7a639ee0b4f342e5c4070d634922f49d9556105c
Kacper Gruszczyński <58849959+Dunkhell@users.noreply.github.com>
2023-11-15 10:27:24 +0100

    lab4.sql
    Skonczono lab4, dodano pakiety

--- lab4.sql
+++ lab4.sql
@@ -1,25 +1,30 @@
+-- zad 3.1
+CREATE OR REPLACE PACKAGE ALL_FUNCTIONS AS
+    FUNCTION get_job_name(job_id IN VARCHAR2) RETURN VARCHAR2;
+    FUNCTION calculate_annual_salary(employee_id IN NUMBER) RETURN NUMBER;
+    FUNCTION extract_area_code(phone_number IN VARCHAR2) RETURN VARCHAR2;
+    FUNCTION capitalize_first_last(input_str IN VARCHAR2) RETURN VARCHAR2;
+    FUNCTION get_employee_department_count(country_name IN VARCHAR2,
+                                           v_department_count OUT NUMBER) RETURN NUMBER;
+end ALL_FUNCTIONS;
+
+CREATE OR REPLACE PACKAGE BODY ALL_FUNCTIONS AS
 -- zad 1.1
-CREATE OR REPLACE FUNCTION get_job_name(job_id IN VARCHAR2) RETURN VARCHAR2 IS
+CREATE OR REPLACE FUNCTION get_job_name(search_job_id IN VARCHAR2) RETURN VARCHAR2 IS
     v_job_name VARCHAR2(50);
 BEGIN
     SELECT job_title
     INTO v_job_name
     FROM JOBS
-    WHERE job_id = get_job_name.job_id;
+    WHERE job_id = get_job_name.search_job_id;
 
     IF v_job_name IS NULL THEN
         RAISE_APPLICATION_ERROR(-20001, 'Praca o podanym ID nie istnieje.');
     END IF;
 
     RETURN v_job_name;
-END;
+END get_job_name;
 
-DECLARE
-    v_job_name VARCHAR2(50);
-BEGIN
-    v_job_name := get_job_name('AD_VP');
-    DBMS_OUTPUT.PUT_LINE('Nazwa pracy: ' || v_job_name);
-END;
 -- zad 1.2
 
 CREATE OR REPLACE FUNCTION calculate_annual_salary(employee_id IN NUMBER) RETURN NUMBER IS
@@ -43,15 +48,7 @@ BEGIN
 
 
     RETURN v_annual_salary;
-END;
-
-DECLARE
-    v_annual_salary NUMBER;
-BEGIN
-    --v_annual_salary := calculate_annual_salary(145);
-    v_annual_salary := calculate_annual_salary(102);
-    DBMS_OUTPUT.PUT_LINE('Roczne zarobki: ' || v_annual_salary);
-END;
+END calculate_annual_salary;
 
 -- zad 1.3
 CREATE OR REPLACE FUNCTION extract_area_code(phone_number IN VARCHAR2) RETURN VARCHAR2 IS
@@ -60,14 +57,7 @@ BEGIN
     v_area_code := SUBSTR(phone_number, 1, INSTR(phone_number, '-') - 1);
 
     RETURN v_area_code;
-END;
-
-DECLARE
-    v_area_code VARCHAR2(10);
-BEGIN
-    v_area_code := extract_area_code('123-456789');
-    DBMS_OUTPUT.PUT_LINE('Numer kierunkowy: ' || v_area_code);
-END;
+END extract_area_code;
 
 -- zad 1.4
 CREATE OR REPLACE FUNCTION capitalize_first_last(input_str IN VARCHAR2) RETURN VARCHAR2 IS
@@ -80,18 +70,7 @@ BEGIN
     v_result_str := v_result_str || INITCAP(SUBSTR(input_str, LENGTH(input_str)));
 
     RETURN v_result_str;
-END;
-
-
-DECLARE
-    v_result_str VARCHAR2(100);
-BEGIN
-    v_result_str := capitalize_first_last('testInput'); -- Przykładowy ciąg znaków
-    DBMS_OUTPUT.PUT_LINE('Wynik: ' || v_result_str);
-END;
-
--- zad 1.5
--- TODO
+END capitalize_first_last;
 
 -- zad 1.6
 CREATE OR REPLACE FUNCTION get_employee_department_count(country_name IN VARCHAR2,
@@ -111,8 +90,47 @@ BEGIN
     END IF;
 
     RETURN v_employee_count;
+END get_employee_department_count;
+END ALL_FUNCTIONS;
+
+
+-- zad 1.1
+DECLARE
+    v_job_name VARCHAR2(50);
+BEGIN
+    v_job_name := get_job_name('AD_VP');
+    DBMS_OUTPUT.PUT_LINE('Nazwa pracy: ' || v_job_name);
 END;
 
+-- zad 1.2
+DECLARE
+    v_annual_salary NUMBER;
+BEGIN
+    --v_annual_salary := calculate_annual_salary(145);
+    v_annual_salary := calculate_annual_salary(102);
+    DBMS_OUTPUT.PUT_LINE('Roczne zarobki: ' || v_annual_salary);
+END;
+
+-- zad 1.3
+DECLARE
+    v_area_code VARCHAR2(10);
+BEGIN
+    v_area_code := extract_area_code('123-456789');
+    DBMS_OUTPUT.PUT_LINE('Numer kierunkowy: ' || v_area_code);
+END;
+
+-- zad 1.4
+DECLARE
+    v_result_str VARCHAR2(100);
+BEGIN
+    v_result_str := capitalize_first_last('testInput'); -- Przykładowy ciąg znaków
+    DBMS_OUTPUT.PUT_LINE('Wynik: ' || v_result_str);
+END;
+
+-- zad 1.5
+-- TODO
+
+-- zad 1.6
 DECLARE
     v_employee_count   NUMBER;
     v_department_count NUMBER;
@@ -226,5 +244,86 @@ BEGIN
     :NEW.max_salary := :OLD.max_salary;
 end;
 
-UPDATE JOBS SET MAX_SALARY = 20000 WHERE JOB_ID = 'AC_ACCOUNT';
-SELECT * FROM JOBS WHERE JOB_ID = 'AC_ACCOUNT';
+UPDATE JOBS
+SET MAX_SALARY = 20000
+WHERE JOB_ID = 'AC_ACCOUNT';
+SELECT *
+FROM JOBS
+WHERE JOB_ID = 'AC_ACCOUNT';
+
+-- zad 3.2
+CREATE OR REPLACE PACKAGE region_package AS
+    PROCEDURE create_region(p_region_id NUMBER, p_region_name VARCHAR2);
+    PROCEDURE update_region(p_region_id NUMBER, p_region_name VARCHAR2);
+    PROCEDURE delete_region(p_region_id NUMBER);
+    FUNCTION get_region_by_id(p_region_id NUMBER) RETURN VARCHAR2;
+    FUNCTION get_all_regions RETURN SYS_REFCURSOR;
+END region_package;
+
+CREATE OR REPLACE PACKAGE BODY region_package AS
+    PROCEDURE create_region(p_region_id NUMBER, p_region_name VARCHAR2) IS
+    BEGIN
+        INSERT INTO REGIONS (REGION_ID, REGION_NAME) VALUES (p_region_id, p_region_name);
+    END create_region;
+
+    PROCEDURE update_region(p_region_id NUMBER, p_region_name VARCHAR2) IS
+    BEGIN
+        UPDATE REGIONS SET REGION_NAME = p_region_name WHERE REGION_ID = p_region_id;
+    END update_region;
+
+    PROCEDURE delete_region(p_region_id NUMBER) IS
+    BEGIN
+        DELETE FROM REGIONS WHERE REGION_ID = p_region_id;
+    END delete_region;
+
+    FUNCTION get_region_by_id(p_region_id NUMBER) RETURN VARCHAR2 IS
+        v_region_name VARCHAR2(25);
+    BEGIN
+        SELECT REGION_NAME INTO v_region_name FROM REGIONS WHERE REGION_ID = p_region_id;
+        RETURN v_region_name;
+    END get_region_by_id;
+
+    FUNCTION get_all_regions RETURN SYS_REFCURSOR IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR SELECT * FROM REGIONS;
+        RETURN v_cursor;
+    END get_all_regions;
+END region_package;
+
+BEGIN
+    region_package.create_region(261, 'Asia');
+    region_package.create_region(262, 'North America');
+    COMMIT;
+END;
+
+BEGIN
+    region_package.update_region(261, 'European Union');
+    COMMIT;
+END;
+
+DECLARE
+    v_region_name VARCHAR2(25);
+BEGIN
+    v_region_name := region_package.get_region_by_id(261);
+    DBMS_OUTPUT.PUT_LINE('Region Name: ' || v_region_name);
+END;
+
+DECLARE
+    v_cursor SYS_REFCURSOR;
+    v_region_id NUMBER;
+    v_region_name VARCHAR2(25);
+BEGIN
+    v_cursor := region_package.get_all_regions;
+    LOOP
+        FETCH v_cursor INTO v_region_id, v_region_name;
+        EXIT WHEN v_cursor%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('Region ID: ' || v_region_id || ', Region Name: ' || v_region_name);
+    END LOOP;
+    CLOSE v_cursor;
+END;
+
+BEGIN
+    region_package.delete_region(262);
+    COMMIT;
+END;
